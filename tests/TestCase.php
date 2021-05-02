23db80ed667758e85b384ff4ed11487c30b2eedb
luke <lukeraymonddowning@gmail.com>
2021-05-02 08:15:57 +0100

    Adds support for php 7.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,7 +33,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-//            RayServiceProvider::class,
+            RayServiceProvider::class,
             MorpherServiceProvider::class,
         ];
     }
