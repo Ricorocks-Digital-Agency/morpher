24e950f44ad0a761f3d4bbdeb4e577484476bf6e
luke <lukeraymonddowning@gmail.com>
2021-05-02 08:17:15 +0100

    Updates github workflow to include windows and 7.4 support.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,7 +33,6 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-            RayServiceProvider::class,
             MorpherServiceProvider::class,
         ];
     }
